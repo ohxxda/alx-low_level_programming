136b12f4b8d98be2bc5d1d676eb737a95be1ba52
ohxxda <ohoodmukhtar4@gmail.com>
2023-08-13 14:51:16 -0700

    back

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function 
+ * Description: A C program that prints with put function
  *
  * Return: Always 0 (succss)
 */
 
 int main(void)
 {
+
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 
