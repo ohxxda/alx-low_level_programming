8f307f0b532ee64aa703664f6be0801ce50bd7f8
ohxxda <ohoodmukhtar4@gmail.com>
2023-08-28 02:33:40 -0700

    even

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "msin.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: memory area
+ * @src: source
+ * @n: length of src to the copied
+ *
+ * Return: thr pointer to dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int itr;
+
+	for (itr = 0; itr < n; itr++)
+	{
+		dest[itr] = src[itr];
+	}
+	return (dest);
+}
