215d0b03af407522d0f08638789d315c1ef3594e
ohxxda <ohoodmukhtar4@gmail.com>
2023-08-28 15:02:29 -0700

    even

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,9 +11,9 @@ void print_chessboard(char (*a)[8])
 {
 	int itr, jtr;
 
-	for (itr = 0; itr < 0; itr++)
+	for (itr = 0; itr < 8; itr++)
 	{
-		for (jtr = 0; jtr < 0; jtr++)
+		for (jtr = 0; jtr < 8; jtr++)
 		{
 			_putchar(a[itr][jtr]);
 		}
